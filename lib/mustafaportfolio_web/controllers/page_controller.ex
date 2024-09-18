407799a698d77d09bbc24671d6256958484c345b
mustafapc <mypcdk1@gmail.com>
2024-09-18 10:28:43 +0300

    this is new commit

--- lib/mustafaportfolio_web/controllers/page_controller.ex
+++ lib/mustafaportfolio_web/controllers/page_controller.ex
@@ -4,6 +4,7 @@ defmodule MustafaportfolioWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
+
     render(conn, :home)
   end
 end
